1c1dbcad7fc35615f11daecf7b7ded3526714039
carlos-arduino <cae_arduino@icloud.com>
2021-03-26 18:15:27 -0300

    Alterado rota consulta metodos pagamento

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -6,7 +6,7 @@ class Payment
   end
 
   def self.all
-    response = Faraday.get('smartflix.com.br/api/v1/payments')
+    response = Faraday.get('http://localhost/api/v1/payment_methods')
     return [] if response.status == 400 || response.status == 500
 
     json_response = JSON.parse(response.body, symbolize_names: true)
