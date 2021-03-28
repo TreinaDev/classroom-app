b9d78a31d07c00283387d3cd4ed639f7921ff41d
Guilherme A.V. Marques <guilhermevidal4857@gmail.com>
2021-03-27 21:41:16 -0300

    WIP agenda de aulas

---
+++ spec/features/customer_see_scheduled_video_class_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+feature 'Customer see scheduled video class' do
+  scenario 'successfully' do
+    aluno = create(:customer)
+
+    customer_plan = Plan.new(
+      name: 'Plano Black',
+      price: 109.90,
+      categories: [
+        {
+          name: 'Bodybuilding'
+        },
+        {
+          name: 'Crossfit'
+        }
+      ]
+    )
+
+    allow(DateTime).to receive(:now) { DateTime.parse '2021-03-17 20:30:00.000000000 -0300' }
+    allow(Plan).to receive(:find_by).with('a2w5q8y10ei')
+                                    .and_return([customer_plan])
+
+    crossfit_teacher = create(:user,
+                              email: 'crossfit@smartflix.com.br',
+                              name: 'João José Silva Santos',
+                              password: '123456')
+    bodybuilding_teacher = create(:user,
+                                  email: 'bodybuilding@smartflix.com.br',
+                                  name: 'João Maria Oliveira dos Santos',
+                                  password: '654321')
+
+    crossfit01 = create(:video_class,
+                        name: 'CrossFit - Aula inaugural',
+                        user: crossfit_teacher,
+                        start_at: '17-03-2021 20:00:00',
+                        end_at: '17-03-2021 21:30:00',
+                        category: 'Crossfit')
+    crossfit02 = create(:video_class,
+                        name: 'CrossFit - Aula 01',
+                        user: crossfit_teacher,
+                        start_at: '18-03-2021 20:00:00',
+                        end_at: '18-03-2021 21:30:00',
+                        category: 'Crossfit')
+    bodybuilding01 = create(:video_class,
+                            name: 'Musculacao - Aula inaugural',
+                            user: bodybuilding_teacher,
+                            start_at: '17-03-2021 20:00:00',
+                            end_at: '17-03-2021 21:30:00',
+                            category: 'Bodybuilding')
+    bodybuilding02 = create(:video_class,
+                            name: 'Musculacao - Aula 01',
+                            user: bodybuilding_teacher,
+                            start_at: '18-03-2021 20:00:00',
+                            end_at: '18-03-2021 21:30:00',
+                            category: 'Bodybuilding')
+
+    login_as aluno, scope: :customer
+
+    # TODO: definir caminho de acesso para agenda
+    visit root_path
+
+    expect(current_path).to eq(scheduled_video_classes_path)
+    within("div\##{crossfit02.category}#{crossfit02.id}") do
+      expect(page).to have_content('CrossFit - Aula 01')
+      expect(page).to have_content(crossfit_teacher.name)
+    end
+    within("div\##{bodybuilding02.category}#{bodybuilding02.id}") do
+      expect(page).to have_content('Musculacao - Aula 01')
+      expect(page).to have_content(bodybuilding_teacher.name)
+    end
+
+    expect(html).not_to have_selector("div\##{crossfit01.category}#{crossfit01.id}")
+    expect(html).not_to have_selector("div\##{bodybuilding01.category}#{bodybuilding01.id}")
+    expect(page).not_to have_content('CrossFit - Aula inaugural')
+    expect(page).not_to have_content('Musculacao - Aula inaugural')
+  end
+end
